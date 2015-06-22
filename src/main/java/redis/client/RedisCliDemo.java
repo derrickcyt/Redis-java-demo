8d50d9a256f33b02feab1efb082fa9dc194fb4f8
derrickcyt <422780332@qq.com>
2015-06-22 20:12:45 +0800

    test
    init

---
+++ src/main/java/redis/client/RedisCliDemo.java
@@ -0,0 +1,13 @@
+package redis.client;
+
+import redis.clients.jedis.Jedis;
+
+public class RedisCliDemo {
+
+	public static void main(String[] args) {
+
+		Jedis jedis = new Jedis("192.168.174.143", 7000);
+		System.out.println(jedis.get("hello"));
+	}
+
+}
