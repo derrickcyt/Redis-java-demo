0bbb471fbebd6e76fd37d829b22ecb03896db539
derrickcyt <422780332@qq.com>
2015-06-23 22:40:08 +0800

    zhushi

--- src/main/java/redis/client/RedisCliDemo.java
+++ src/main/java/redis/client/RedisCliDemo.java
@@ -6,6 +6,7 @@ public class RedisCliDemo {
 
 	public static void main(String[] args) {
 
+		// ip+port
 		Jedis jedis = new Jedis("192.168.174.143", 7000);
 		System.out.println(jedis.get("hello"));
 	}
